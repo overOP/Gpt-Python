eb757f871fbdbdc45a4cbff31477bc7310862659
Pradeep Chaudhary <cgpradeepop@gmail.com>
2026-07-20 17:37:33 +0545

    Refactor mine.py: add wish function and improve area_of_circle output

--- mine.py
+++ mine.py
@@ -1,7 +1,23 @@
+def wish(*names):
+    for name in names:
+        print("Hello", name)
+
+wish("Pradeep", "Over")
+
 pi = 3.14
 def area_of_circle(r):
-    return pi * r * r
+    return pi * r *r 
+r = int(input("Enter the radius of circle: "))
+print("Area of circle is: ", area_of_circle(r))
 
-r = int(input("Enter the radius of the circle: "))
-area = area_of_circle(r)
-print("The area of the circle with radius", r, "is: ", area)
+def calculete(a, b):
+    total = a + b
+    diff = a - b
+    prod = a * b
+    div = a / b
+    mod = a % b
+    return total, diff, prod, div, mod
+a = int(input("Enter a value: "))
+b = int(input("Enter b value: "))
+s,d,p,q,m = calculete(a,b)
+print("Total= ",s,"Diff= ",d,"Prod= ",p,"Div= ",q,"Mod= ",m)
